9e480df9566a0b4fa31c411eac732657759a1a3a
abhishekkr <abhishk@thoughtworks.com>
2011-08-19 20:27:14 +0530

    added a method to check correctness of XML and improved node-ification

--- rubysrc/parser.rb
+++ rubysrc/parser.rb
@@ -3,22 +3,52 @@
 def _splitter_ (xmldata, pattern)
 end
 
+def xml_correct? (node)
+  correct=true
+  xml={}
+  node.each do |n|
+    if n.class==String then
+      next
+    elsif n.first.include?('/') then
+      tag=n.first.split('/')[1].strip
+      if xml[tag]==tag
+        xml[tag]="<#{tag}/>"
+      else
+	xml[tag]=tag
+      end
+    else
+      tag=n.first.strip.split(' ')[0]
+      xml[tag]=tag
+    end
+  end
+  xml.each do |key, value|
+    if key==value
+      puts key + " node is faulty..."
+      correct=false
+    end
+  end
+  correct
+end
+
 def get_nodes (node)
-  xml=[]
-  if node.first.class==String then
-    p node.first #xml[:"#{node.first}"]=" "
-    xml.push get_nodes(node[1..-1]) unless node.length==1
-  elsif node.first.first.include?('/') then
-    #xml[:"#{node.first}"]= parse(node[1..-1].to_s) unless node==nil
-    xml.push get_nodes(node[1..-1]) unless node.length==1
-  else
-    p node.first.first #xml[:"#{node.first}"]=" "
-    xml.push get_nodes(node[1..-1]) unless node.length==1
+  return unless xml_correct? node
+  xml={}
+  node.each do |n|
+    if n.class==String then
+      xml["~"]=n
+    elsif n.first.include?('/') then
+      tag=n.first.split('/')[1].strip
+      xml[tag]+="</#{tag}>" 
+    else
+      tag=n.first.strip.split(' ')[0]
+      xml[tag]="<#{tag}>"
+      xml[tag]+=n[1..-1].to_s unless n.length==1
+    end
   end
   xml
 end
 
-def parse(xmldata)
+def xml_handler(xmldata)
   node=[xmldata.split(/</)[0]]
   xmldata.split(/</)[1..-1].each do |x1|
     node.push x1.split(/>/)
@@ -30,10 +60,12 @@ def parse(xmldata)
     node.push _splitter_(x1, '>')
   end
 =end
-p node
-#return
+  puts "|"*25
+  puts "for xml:\n" +  xmldata
+  puts "~"*25
   xml=get_nodes node
   p xml
+  puts "|"*25
 end
 
 ARGV.each do |args|
@@ -43,8 +75,8 @@ ARGV.each do |args|
   rescue
     p args + " can't be read... using deafult content here"
   end
-
-  parse content
+  xml_handler content
 end
 
-parse "HTML Eg <html> <head> <title>top bar </title></head><body> here is<h1>BODY</h1> tag</body> </html>" if ARGV.length==0
+xml_handler "HTML Eg <html> <head> <title>top bar </title></head><body style=\"color:green;\"> here is<h1>BODY</h1> tag</body> </html>" if ARGV.length==0
+xml_handler "HTML Eg <html> <head> <title>top bar </title><body style=\"color:green;\"> here is BODY</h1> tag</body> </html>" if ARGV.length==0
