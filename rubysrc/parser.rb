00f04ddc99e20a1a79d2fdb816e31dab385b4aa6
abhishekkr <abhishk@thoughtworks.com>
2011-12-25 12:02:21 +0530

    some bug fixed like assigning xmlnodes & xmltags; more of code refactoring

--- rubysrc/parser.rb
+++ rubysrc/parser.rb
@@ -1,39 +1,48 @@
 #!/usr/bin/env ruby
 
+module XMLStdout
+  def self._err(mesag); puts "ERROR:: #{mesag}"; end
+  def self._nfo(mesag); puts "INFORMATION:: #{mesag}"; end
+end
+
 module XMLIndexHandler
   def self.get_node_indexes(xml_motor, tag)
-    xml_idx_to_find = []
+   xml_idx_to_find = []
+   begin
     xml_motor.xmltags[tag.split(".")[0]].each_value {|val|  xml_idx_to_find.push val }
     xml_idx_to_find = xml_idx_to_find.flatten
 
     tag.split(".")[1..-1].each do |tag_i|
       outer_idx = xml_idx_to_find
-      osize=outer_idx.size/2 -1
-      x_curr=[]
+      x_curr = []
       xml_motor.xmltags[tag_i].each_value {|val|  x_curr.push val }
       x_curr = x_curr.flatten
-      xsize=x_curr.size/2 -1
 
-      xml_idx_to_find = expand_node_indexes outer_idx, osize, x_curr, xsize
+      xml_idx_to_find = expand_node_indexes outer_idx, x_curr
     end
-    xml_idx_to_find
+   rescue
+    XMLStdout._err "Finding index for tag:#{tag}.\nLook if it's actually present in the provided XML."
+   end
+   xml_idx_to_find
   end
 
-  def self.expand_node_indexes(outer_idx, osize, x_curr, xsize)
-    exapnded_node_indexes = []
+  def self.expand_node_indexes(outer_idx, x_curr)
+    osize = outer_idx.size/2 -1
+    xsize = x_curr.size/2 -1
+    expanded_node_indexes = []
     0.upto osize do |o|
-      o1=outer_idx[o*2]
-      o2=outer_idx[o*2 +1]
+      o1 = outer_idx[o*2]
+      o2 = outer_idx[o*2 +1]
       0.upto xsize do |x|
-        x1=x_curr[x*2]
-        x2=x_curr[x*2 +1]
+        x1 = x_curr[x*2]
+        x2 = x_curr[x*2 +1]
         unless o1>x1 or o2<x2
-          exapnded_node_indexes.push x1
-          exapnded_node_indexes.push x2
+          expanded_node_indexes.push x1
+          expanded_node_indexes.push x2
         end
       end
     end
-    exapnded_node_indexes.flatten
+    expanded_node_indexes.flatten
   end
 end
 
@@ -71,7 +80,7 @@ end
 module XMLJoiner
   def self.dejavu_attributes(attrib_hash)
     return nil if attrib_hash.nil?
-    attributes=""
+    attributes = ""
     attrib_hash.each_key do |hash_key|
       attributes += " " + hash_key + "=" + attrib_hash[hash_key]
     end
@@ -84,7 +93,7 @@ end
 
 module XMLMotorEngine
   def self._splitter_(xmldata)
-    @xmlnodes=[xmldata.split(/</)[0]]
+    @xmlnodes = [xmldata.split(/</)[0]]
     xmldata.split(/</)[1..-1].each do |x1|
       @xmlnodes.push  XMLChopper.get_tag_attrib_value(x1)
     end
@@ -96,7 +105,7 @@ module XMLMotorEngine
     idx = 1
     depth = 0
     @xmlnodes[1..-1].each do |xnode|
-      tag_name = xnode[0][0]
+      tag_name = xnode[0][0].strip
       unless tag_name.match(/^\/.*/) then
         @xmltags[tag_name] ||= {}
         @xmltags[tag_name][depth] ||= []
@@ -129,7 +138,7 @@ module XMLMotorEngine
       nodes[ncount] += @xmlnodes[node_start][1] unless @xmlnodes[node_start][1].nil?
       (node_start+1).upto (node_stop-1) do |node_idx|
         any_attrib ||= ""
-        any_attrib =  XMLJoiner.dejavu_attributes(@xmlnodes[node_idx][0][1]).to_s unless @xmlnodes[node_idx][0][1].nil?
+        any_attrib = XMLJoiner.dejavu_attributes(@xmlnodes[node_idx][0][1]).to_s unless @xmlnodes[node_idx][0][1].nil?
         nodes[ncount] += "<" + @xmlnodes[node_idx][0][0] + any_attrib + ">"
         nodes[ncount] += @xmlnodes[node_idx][1] unless @xmlnodes[node_idx][1].nil?
       end
@@ -140,14 +149,15 @@ module XMLMotorEngine
 
   def self.xml_extracter(tag_to_find=nil, attrib_to_find=nil)
     my_nodes = nil
-    if attrib_to_find.nil?
+    if attrib_to_find.nil? and tag_to_find.nil?
+    elsif attrib_to_find.nil?
       xml_to_find = XMLIndexHandler.get_node_indexes self, tag_to_find
       my_nodes = _grab_my_node_ xml_to_find
     elsif tag_to_find.nil? 
       #
-      #just attrib-based search to come
+      XMLStdout._nfo "Just attrib-based search to come"
       #
-    elsif !attrib_to_find.nil? and !tag_to_find.nil?
+    else
       xml_to_find = XMLIndexHandler.get_node_indexes self, tag_to_find
       my_nodes = _grab_my_node_ xml_to_find, attrib_to_find
     end
@@ -155,17 +165,20 @@ module XMLMotorEngine
   end  
 
   def self.xml_miner(xmldata, tag_to_find=nil, attrib_to_find=nil)
+    return nil if xmldata.nil?
     _splitter_ xmldata
     _indexify_
     xml_extracter tag_to_find, attrib_to_find
   end 
 
-  def self.xmlnodes(xmlnodes=nil)
-    @xmlnodes ||= xmlnodes
+  def self.xmlnodes(xml_nodes=nil)
+    @xmlnodes = xml_nodes unless xml_nodes.nil?
+    @xmlnodes
   end
 
-  def self.xmltags(xmltags=nil)
-    @xmltags ||= xmltags
+  def self.xmltags(xml_tags=nil)
+    @xmltags = xml_tags unless xml_tags.nil?
+    @xmltags
   end
 
   def self.pre_processed_content(_nodes, _tags=nil, tag_to_find=nil, attrib_to_find=nil)
@@ -174,13 +187,13 @@ module XMLMotorEngine
       unless _tags.nil?
         xmltags _tags
       else
-        _indexify_ _nodes
+        _indexify_ 
       end
       return xml_extracter tag_to_find, attrib_to_find
     rescue
-      puts "Error problem parsing processed XML Nodes."
+      XMLStdout._err "Parsing processed XML Nodes."
     end
-    return ""
+    return nil 
   end
 end
 
@@ -192,7 +205,7 @@ module XMLMotor
     begin
       return get_node_from_content(File.read(file.to_s), my_tag, my_attrib) if File.readable? file.to_s
     rescue
-      puts "Error: #{file} is not readable."
+      XMLStdout._err "#{file} is not readable."
     end
     return ""
   end
@@ -201,7 +214,7 @@ module XMLMotor
     begin
       return XMLMotorEngine.xml_miner content, my_tag, my_attrib unless content.nil?
     rescue
-      puts "Error problem parsing String Content #{content}"
+      XMLStdout._err "Parsing String Content #{content}"
     end
     return ""
   end
