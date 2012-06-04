235a6fcdc563955ffc41b3efdceabfa85ea806b6
AbhishekKr <abhikumar163@gmail.com>
2012-06-05 00:02:18 +0530

    VERBOSE=err would only give run-time messages, else default is silent

--- ruby/xml-motor/xml-stdout.rb
+++ ruby/xml-motor/xml-stdout.rb
@@ -1,4 +1,10 @@
 module XMLStdout
-  def self._err(mesag); puts "ERROR:: #{mesag}"; end
-  def self._nfo(mesag); puts "INFORMATION:: #{mesag}"; end
+
+  def self._err(mesag)
+    puts "ERROR:: #{mesag}" unless ENV['VERBOSE'].nil?
+  end
+
+  def self._nfo(mesag)
+    puts "INFO:: #{mesag}" unless ENV['VERBOSE'].nil?
+  end
 end
